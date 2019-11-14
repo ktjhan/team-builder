4b38ed3850fe1941eba52a0d95fa7f7131831b13
ktjhan <tjhan.kavin@gmail.com>
2019-11-13 20:13:28 -0800

    finiahed

---
+++ src/Data.js
@@ -0,0 +1,23 @@
+const Member = [ 
+  {
+    name: 'Iron Man', 
+    email: 'TonyStark32@gmail.com',  
+    role: 'Front-end Engineer', 
+    image: 'http://www.pngall.com/wp-content/uploads/2016/05/Iron-Man-PNG-Picture.png' 
+  },
+  {    
+    name: 'Captain America', 
+    email: 'Captain.1776@gmail.com', 
+    role: 'Back-end Engineer', 
+    image: 'https://pngriver.com/wp-content/uploads/2018/03/Download-Captain-America-PNG-Photo-For-Designing-Projects-1000x807.png' 
+  },
+   {
+     name: 'Spider-Man', 
+     email: 'Peter.Parker@gmail.com', 
+     role: 'UX Designer', 
+     image: 'http://pngimg.com/uploads/spider_man/spider_man_PNG84.png' 
+   }, 
+
+]
+
+export default Member; 
